04eac8ac84835dafba900c2cff06e5ab0fb0dd71
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-07-18 12:20:44 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function eventFocus(){
+        console.log("Good!")
+    }
+    function eventblur(){
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <button onFocus={eventFocus} onBlur ={eventblur} >Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
